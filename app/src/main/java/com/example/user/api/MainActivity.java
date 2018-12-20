6b73b3651f6566f20e77f5423c0a44877a22d5ad
105360093 <iamsam06@yahoo.com.hk>
2018-12-20 15:14:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/api/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.user.api;
+
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    private BroadcastReceiver recviver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Data data = new Gson().fromJson(intent.getExtras().getString("json"), Data.class);
+
+            final String[] items = new String[data.result.results.length];
+            for(int i=0; i<items.length; i++)
+                items[i] = "\n列車即將進入 :"+data.result.results[i].Station +
+                        "\n列車行駛目的地 :" + data.result.results[i].Destination;
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("台北捷運列車到站名")
+                            .setItems(items,null)
+                            .show();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerReceiver(recviver,new IntentFilter("MyMessage"));
+
+        button=findViewById(R.id.btn_query);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request req = new Request.Builder()
+                        .url("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b").build();
+                new OkHttpClient().newCall(req).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        Log.e("查詢失敗",e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                    sendBroadcast(new Intent("MyMessage").putExtra("json",response.body().string()));
+                    }
+                });
+            }
+        });
+    }
+}
+
+class Data{
+    Result result;
+
+    class Result{
+        Results[] results;
+
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
+
